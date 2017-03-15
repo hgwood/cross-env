d33a33aa4546ece1235507c32ef321dd192e5cfd
Hugo Wood <hugo.wood-ext@vif.fr>
2017-03-15 16:43:37 +0100

    wip (works but maybe too far from original code)

--- src/command.js
+++ src/command.js
@@ -7,19 +7,19 @@ export default commandConvert
  * @param {String} command Command to convert
  * @returns {String} Converted command
  */
-function commandConvert(command, name = false) {
-  const envUseUnixRegex = /\$(\w+)|\${(\w+)}/g // $my_var or ${my_var}
+function commandConvert(command, replacer) {
+  const envUseUnixRegex = /\$(\w+)|\${(\w+)}|%(.*?)%/g // $my_var or ${my_var} or %my_var%
   const envUseWinRegex = /%(.*?)%/g // %my_var%
   const isWin = isWindows()
   const envExtract = isWin ? envUseUnixRegex : envUseWinRegex
-  if (name) {
-    console.log(command)
-    return command.replace(
-      isWin ? envUseWinRegex : envUseUnixRegex,
-      (match, $1, $2) =>
-        console.log('lol', match, $1, $2) || process.env[$1 || $2],
-    )
-  } else {
-    return command.replace(envExtract, isWin ? '%$1$2%' : '$$$1')
-  }
+  // const envExtract = envUseUnixRegex
+  const strReplace = isWin ? '%$1$2%' : '$$$1'
+  return command.replace(
+    envExtract,
+    replacer ?
+      (match, $1, $2) => {
+        return replacer(isWin, $1 || $2)
+      } :
+      strReplace,
+  )
 }
