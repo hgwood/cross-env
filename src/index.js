563323e5e66ebb5fde31418db437911dd849b435
Hugo Wood <hugo.wood-ext@vif.fr>
2017-03-15 14:39:16 +0100

    wip (works)

--- src/index.js
+++ src/index.js
@@ -7,21 +7,24 @@ const envSetterRegex = /(\w+)=('(.+)'|"(.+)"|(.+))/
 
 function crossEnv(args) {
   const [command, commandArgs, env] = getCommandArgsAndEnvVars(args)
-  if (command) {
-    const proc = spawn(command, commandArgs, {stdio: 'inherit', env})
-    process.on('SIGTERM', () => proc.kill('SIGTERM'))
-    process.on('SIGINT', () => proc.kill('SIGINT'))
-    process.on('SIGBREAK', () => proc.kill('SIGBREAK'))
-    process.on('SIGHUP', () => proc.kill('SIGHUP'))
-    proc.on('exit', process.exit)
-    return proc
-  }
+  console.log('args', args)
+  console.log('out', command, commandArgs, env.MY_VAR)
+  // if (command) {
+  //   const proc = spawn(command, commandArgs, {stdio: 'inherit', env})
+  //   process.on('SIGTERM', () => proc.kill('SIGTERM'))
+  //   process.on('SIGINT', () => proc.kill('SIGINT'))
+  //   process.on('SIGBREAK', () => proc.kill('SIGBREAK'))
+  //   process.on('SIGHUP', () => proc.kill('SIGHUP'))
+  //   proc.on('exit', process.exit)
+  //   return proc
+  // }
   return null
 }
 
 function getCommandArgsAndEnvVars(args) {
   const envVars = getEnvVars()
-  const commandArgs = args.map(commandConvert)
+  const commandArgs = args.map(arg => commandConvert(arg))
+  console.log('commandArgs', commandArgs)
   const command = getCommand(commandArgs, envVars)
   return [command, commandArgs, envVars]
 }
@@ -31,7 +34,10 @@ function getCommand(commandArgs, envVars) {
     const shifted = commandArgs.shift()
     const match = envSetterRegex.exec(shifted)
     if (match) {
-      envVars[match[1]] = match[3] || match[4] || match[5]
+      envVars[match[1]] = commandConvert(
+        match[3] || match[4] || match[5],
+        true,
+      )
     } else {
       return shifted
     }
